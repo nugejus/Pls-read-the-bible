4c614be44827831a3bf5f59044cb2b255fa183e8
Seungju <nugejus@mail.ru>
2025-12-09 12:56:01 +0300

    fix:debugmode off

--- server.py
+++ server.py
@@ -6,4 +6,4 @@ from config import PORT
 
 if __name__ == "__main__":
     server = create_flask_app()
-    server.run(host="0.0.0.0", port=PORT, debug=True)
+    server.run(host="0.0.0.0", port=PORT, debug=False)
